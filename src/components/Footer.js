cbdc0d73ae7cfd8ea4cdb405af3503da882be449
pablomailhet <106112558+pablomailhet@users.noreply.github.com>
2022-05-26 17:02:23 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="bg-light text-center text-lg-start">
             <div className="text-center p-3" >
-                &copy; 2022 RC_3DPrint
+                &copy; {(new Date().getFullYear())} RC_3DPrint
             </div>
         </footer>
     );
