e93e9cd22f4d28d412e58a556203052246bb91cf
pablomailhet <106112558+pablomailhet@users.noreply.github.com>
2022-05-26 16:56:49 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
 
     return (
-        <footer class="bg-light text-center text-lg-start">
-            <div class="text-center p-3" >
+        <footer className="bg-light text-center text-lg-start">
+            <div className="text-center p-3" >
                 &copy; 2022 RC_3DPrint
             </div>
         </footer>
