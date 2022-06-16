6109a5a2091138864f157ce7b345f32162058699
pablomailhet <106112558+pablomailhet@users.noreply.github.com>
2022-06-16 16:59:35 -0300

    Delete src/components directory

--- src/components/CartWidget.js
+++
@@ -1,8 +0,0 @@
-const CartWidget = () => {
-    return (
-        <span className="material-symbols-outlined text-success">
-            shopping_cart
-        </span>
-    );
-};
-export default CartWidget;
